5ee463c935659c942467654d31bf94e660568f48
Shunnie816 <ponyo.shun0816@gmail.com>
2024-05-18 19:44:36 +0900

    modify Top

--- frontend/src/components/pages/Top/containers/index.tsx
+++ frontend/src/components/pages/Top/containers/index.tsx
@@ -6,7 +6,8 @@ import { ThemeProvider } from "@mui/material/styles";
 import styles from "./index.module.scss";
 import { Icon } from "@/components/parts/Icon";
 import { TodoList } from "../../TodoList/containers";
-import { theme } from "@/components/themes";
+import { defaultTheme, tabTheme } from "@/components/themes";
+import { CompanySearchList } from "../../CompanySearchList/containers";
 
 export default function Top() {
   const [tabValue, setTabValue] = useState<number>(0);
@@ -31,33 +32,40 @@ export default function Top() {
       <Box
         sx={{ flexGrow: 1, width: "100%", position: "fixed", zIndex: "100" }}
       >
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={defaultTheme}>
           <AppBar position="static" className={styles.appBar}>
             <div className={styles.toolBar}>
-              <Typography color="secondary" variant="h6" component="a" href="/">
+              <Typography
+                color="text.primary"
+                variant="h6"
+                component="a"
+                href="/"
+              >
                 MA Tool
               </Typography>
-              <Tabs
-                value={tabValue}
-                onChange={handleChange}
-                aria-label="basic tabs example"
-                textColor="secondary"
-                indicatorColor="secondary"
-                className={styles.tabs}
-              >
-                {navList.map((listItem) => {
-                  return (
-                    <Tab
-                      key={listItem.id}
-                      icon={listItem.icon}
-                      iconPosition="start"
-                      label={listItem.label}
-                      id={`tabId-${listItem.id}`}
-                      aria-controls={`tabpanel-${listItem.id}`}
-                    />
-                  );
-                })}
-              </Tabs>
+              <ThemeProvider theme={tabTheme}>
+                <Tabs
+                  value={tabValue}
+                  onChange={handleChange}
+                  aria-label="basic tabs example"
+                  textColor="primary"
+                  indicatorColor="primary"
+                  className={styles.tabs}
+                >
+                  {navList.map((listItem) => {
+                    return (
+                      <Tab
+                        key={listItem.id}
+                        icon={listItem.icon}
+                        iconPosition="start"
+                        label={listItem.label}
+                        id={`tabId-${listItem.id}`}
+                        aria-controls={`tabpanel-${listItem.id}`}
+                      />
+                    );
+                  })}
+                </Tabs>
+              </ThemeProvider>
             </div>
           </AppBar>
         </ThemeProvider>
@@ -67,7 +75,7 @@ export default function Top() {
           <TodoList />
         </TabContentFrame>
         <TabContentFrame value={tabValue} index={1}>
-          会社一覧のページ
+          <CompanySearchList />
         </TabContentFrame>
         <TabContentFrame value={tabValue} index={2}>
           行動履歴のページ
