b3b0e03cc9f98458f3f96dbcc0371406e8f9feaa
Shunnie816 <ponyo.shun0816@gmail.com>
2024-05-18 18:02:15 +0900

    modify icon component

--- frontend/src/components/parts/Icon/index.tsx
+++ frontend/src/components/parts/Icon/index.tsx
@@ -1,31 +1,19 @@
 import React, { FC } from "react";
 import { IconProps } from "@mui/material/Icon";
-import ElectricBoltIcon from "@mui/icons-material/ElectricBolt";
-import AutoGraphOutlinedIcon from "@mui/icons-material/AutoGraphOutlined";
-import SearchIcon from "@mui/icons-material/Search";
 import ListAltIcon from "@mui/icons-material/ListAlt";
-import FlagOutlinedIcon from "@mui/icons-material/FlagOutlined";
-import QueryStatsIcon from "@mui/icons-material/QueryStats";
+import SearchIcon from "@mui/icons-material/Search";
+import AutoGraphOutlinedIcon from "@mui/icons-material/AutoGraphOutlined";
 
-export type IconType =
-  | "AISales"
-  | "sales"
-  | "search"
-  | "list"
-  | "flag"
-  | "analysis";
+export type IconType = "list" | "search" | "sales";
 type BaseProps = {
   icon: IconType;
 };
 type Props = BaseProps & Pick<IconProps, "color" | "fontSize">;
 
 const iconMap = {
-  AISales: ElectricBoltIcon,
-  sales: AutoGraphOutlinedIcon,
   search: SearchIcon,
   list: ListAltIcon,
-  flag: FlagOutlinedIcon,
-  analysis: QueryStatsIcon,
+  sales: AutoGraphOutlinedIcon,
 };
 
 export const Icon: FC<Props> = ({ icon, color, fontSize }) => {
