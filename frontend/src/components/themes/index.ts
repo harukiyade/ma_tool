c4c4eaa25b0a4cbd5024ec08454d40583f6b4645
Shunnie816 <ponyo.shun0816@gmail.com>
2024-05-18 19:44:21 +0900

    update MUI theme

--- frontend/src/components/themes/index.ts
+++ frontend/src/components/themes/index.ts
@@ -1,15 +1,32 @@
 import { createTheme } from "@mui/material";
 
+/**
+ * MUIのpalette
+ * https://mui.com/material-ui/customization/default-theme/?expand-path=$.palette
+ */
+
 /** MUIのカスタムthemeを定義 */
-export const theme = createTheme({
+export const defaultTheme = createTheme({
   palette: {
     primary: {
       main: "#fff200", //yellow
-      light: "#d6e6f2", //lightblue
     },
     secondary: {
-      main: "#303841", //blackNavy
-      light: "#f5f5f5", //white
+      main: "#d6e6f2", //lightblue
+    },
+    text: {
+      primary: "#303841", //blackNavy
+    },
+    background: {
+      default: "#f5f5f5", //white
+    },
+  },
+});
+
+export const tabTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#303841",
     },
   },
 });
