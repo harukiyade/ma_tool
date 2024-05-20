e992e8c88bdbf5bd6dd62803d29290e3af634321
Shunnie816 <ponyo.shun0816@gmail.com>
2024-05-20 09:03:09 +0900

    add Typography size definition

--- frontend/src/components/themes/index.ts
+++ frontend/src/components/themes/index.ts
@@ -21,6 +21,38 @@ export const defaultTheme = createTheme({
       default: "#f5f5f5", //white
     },
   },
+  typography: {
+    h1: {
+      fontSize: 24,
+    },
+    h2: {
+      fontSize: 20,
+    },
+    h3: {
+      fontSize: 18,
+    },
+    h4: {
+      fontSize: 16,
+    },
+    h5: {
+      fontSize: 14,
+    },
+    h6: {
+      fontSize: 12,
+    },
+    subtitle1: {
+      fontSize: 12,
+    },
+    subtitle2: {
+      fontSize: 12,
+    },
+    body1: {
+      fontSize: 12,
+    },
+    body2: {
+      fontSize: 12,
+    },
+  },
 });
 
 export const tabTheme = createTheme({
