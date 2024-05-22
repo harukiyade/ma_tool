515c8bbd60532b00877cc454d4d5ba2923fb55ed
Shunnie816 <ponyo.shun0816@gmail.com>
2024-05-22 09:05:36 +0900

    add side pannel to TodoList

---
+++ frontend/src/components/pages/TodoList/presentations/ListItem/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  ListItem as MUIListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import { Icon, IconType } from "@/components/parts/Icon";
+
+type Props = {
+  text: string;
+  secondaryText?: string;
+  icon?: IconType;
+};
+
+export const ListItem = ({ text, secondaryText, icon }: Props) => {
+  return (
+    <MUIListItem disablePadding>
+      <ListItemButton>
+        {icon && (
+          <ListItemIcon>
+            <Icon icon={icon} />
+          </ListItemIcon>
+        )}
+        <ListItemText secondary={secondaryText}>
+          <Typography>{text}</Typography>
+        </ListItemText>
+      </ListItemButton>
+    </MUIListItem>
+  );
+};
