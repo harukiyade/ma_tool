1c9f591315e87b45c301b8cb7219a937bd032cdf
Shunnie816 <ponyo.shun0816@gmail.com>
2024-05-24 08:25:15 +0900

    create ActionRecord

---
+++ frontend/src/components/pages/ActionRecord/presentations/ActionCard/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
+type Props = {
+  title: string;
+  date: string;
+  content: string;
+};
+
+export const ActionCard = ({ title, date, content }: Props) => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={title}
+        subheader={date}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {content}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
