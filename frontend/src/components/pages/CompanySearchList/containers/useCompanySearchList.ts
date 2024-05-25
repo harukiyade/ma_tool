261f3719bd5e831151d12825d5194c057aedd245
Shunnie816 <ponyo.shun0816@gmail.com>
2024-05-25 20:24:18 +0900

    modify enable all the params

--- frontend/src/components/pages/CompanySearchList/containers/useCompanySearchList.ts
+++ frontend/src/components/pages/CompanySearchList/containers/useCompanySearchList.ts
@@ -1,5 +1,6 @@
 import useSWR from "swr";
-import { Corporate } from "@/api/types";
+import { Corporate } from "@/api/corporate/ResTypes";
+import { CorporateQueryParams } from "@/api/corporate/ReqTypes";
 
 const fetcher = async (url: string) => {
   const response = await fetch(url);
@@ -9,10 +10,25 @@ const fetcher = async (url: string) => {
   return response.json();
 };
 
-const useCompanySearchList = (name: string) => {
+const useCompanySearchList = (params: CorporateQueryParams) => {
   const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+  // パラメータオブジェクトをクエリ文字列に変換する関数
+  const queryParamsToString = (params: CorporateQueryParams): string => {
+    const queryParams = new URLSearchParams();
+    Object.entries(params).forEach(([key, value]) => {
+      if (value !== undefined && value !== null && value !== "") {
+        queryParams.append(key, value);
+      }
+    });
+    return queryParams.toString();
+  };
+
+  /** クエリパラメータとして使用できる文字列に変換(パラメータはURLエンコード化される) */
+  const query = queryParamsToString(params);
+
   const { data, error, isLoading } = useSWR<Corporate>(
-    `${backendUrl}/api/corporate?name=${name}`,
+    `${backendUrl}/api/corporate?${query}`,
     fetcher,
     {
       onSuccess(data) {
