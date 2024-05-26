ec72ce0ac9eb973b9df056d73da760e292036726
Shunnie816 <ponyo.shun0816@gmail.com>
2024-05-26 11:26:22 +0900

    modify layout

--- frontend/src/components/pages/CompanySearchList/containers/index.tsx
+++ frontend/src/components/pages/CompanySearchList/containers/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { CompanyList } from "../presentations/CompanyList";
 import styles from "./index.module.scss";
-import { ThemeProvider, Typography } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
 import { defaultTheme } from "@/components/themes";
 import useCompanySearchList from "./useCompanySearchList";
+import { SearchPannel } from "../presentations/SearchPannel";
 
 export type Company = {
   name: string;
@@ -22,21 +23,7 @@ export const CompanySearchList = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
       <div className={styles.container}>
-        <Typography
-          variant="h1"
-          fontWeight="bold"
-          sx={{ color: "text.primary" }}
-        >
-          会社一覧のページ
-        </Typography>
-        <div>会社情報取得テスト：</div>
-        {data?.["hojin-infos"]?.map((company: any) => (
-          <div key={company.id}>
-            <h2>{company.name}</h2>
-            <p>住所: {company.location}</p>
-            {/* 他のデータも適宜表示 */}
-          </div>
-        ))}
+        <SearchPannel />
         <CompanyList />
       </div>
     </ThemeProvider>
--- frontend/src/components/pages/CompanySearchList/presentations/ListItem/index.tsx
+++ frontend/src/components/pages/CompanySearchList/presentations/ListItem/index.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Company } from "../../containers";
-import { Divider, ListItem as MUIListItem, Typography } from "@mui/material";
+import {
+  Divider,
+  IconButton,
+  ListItem as MUIListItem,
+  Typography,
+} from "@mui/material";
 import { Icon } from "@/components/parts/Icon";
-import { SubInfoItem } from "../SubInfoItem";
 import styles from "./index.module.scss";
 
 type Props = {
@@ -10,48 +14,32 @@ type Props = {
 };
 
 export const ListItem = ({ company }: Props) => {
-  const {
-    name,
-    id,
-    industry,
-    address,
-    establishmentYear,
-    representativeName,
-    capital,
-  } = company;
+  const { name, address } = company;
 
   return (
     <>
       <MUIListItem className={styles.container}>
         <div className={styles.mainInfo}>
           <Typography
-            variant="h2"
-            paragraph
+            variant="h3"
             fontWeight="bold"
             sx={{ color: "text.primary" }}
           >
             {name}
           </Typography>
-          <Typography variant="subtitle1" sx={{ color: "text.secondary" }}>
-            企業ID：{id}
+          <Typography
+            variant="subtitle1"
+            component="dd"
+            sx={{ color: "text.primary" }}
+          >
+            {address}
           </Typography>
         </div>
         <div className={styles.icons}>
-          <Icon icon="copy" />
+          <IconButton>
+            <Icon icon="newTab" />
+          </IconButton>
         </div>
-        <dl className={styles.subInfoList}>
-          <SubInfoItem title="業種" data={industry ?? "-"} />
-          <SubInfoItem title="住所" data={address ?? "-"} />
-          <SubInfoItem
-            title="設立年度"
-            data={establishmentYear ?? "-"}
-            dataText="年"
-          />
-        </dl>
-        <dl className={styles.subInfoList}>
-          <SubInfoItem title="代表者名" data={representativeName ?? "-"} />
-          <SubInfoItem title="資本金等" data={capital ?? "-"} dataText="円" />
-        </dl>
       </MUIListItem>
       <Divider />
     </>
