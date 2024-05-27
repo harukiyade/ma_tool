07ed7fa62dc502fcacb24439f320452e45edeff5
Shunnie816 <ponyo.shun0816@gmail.com>
2024-05-27 09:22:42 +0900

    create url map

---
+++ frontend/src/libs/constants/url.ts
@@ -0,0 +1,23 @@
+/** URLマッピングを管理する */
+export const URL_VALUES = {
+  /**
+   * トップページ
+   */
+  route: "/",
+  /**
+   * アプローチリスト
+   */
+  approach: "/approach",
+  /**
+   * 会社一覧
+   */
+  company: "/company",
+  /**
+   * 行動履歴
+   */
+  history: "/history",
+  /**
+   * 日報作成
+   */
+  reports: "/reports",
+};
