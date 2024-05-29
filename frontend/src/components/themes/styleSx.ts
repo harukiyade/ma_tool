9919a27afa2fb0399d2819ebd080b1880b6f6916
Shunnie816 <ponyo.shun0816@gmail.com>
2024-05-29 09:00:08 +0900

    add file for collecting sx

---
+++ frontend/src/components/themes/styleSx.ts
@@ -0,0 +1,10 @@
+export const textFieldSx = {
+  "& .MuiInputLabel-root.Mui-focused": {
+    color: "primary.dark", // フォーカス時のラベル色
+  },
+  "& .MuiOutlinedInput-root": {
+    "&.Mui-focused fieldset": {
+      borderColor: "primary.dark", // フォーカス時のアウトライン色
+    },
+  },
+};
