358bd63f49fed452dec8224be201ca17b92ba9f5
Shunnie816 <ponyo.shun0816@gmail.com>
2024-05-30 09:12:19 +0900

    add otherInfo input

--- frontend/src/components/themes/styleSx.ts
+++ frontend/src/components/themes/styleSx.ts
@@ -8,3 +8,31 @@ export const textFieldSx = {
     },
   },
 };
+
+export const textFieldSecondarySx = {
+  "& .MuiInputLabel-root.Mui-focused": {
+    color: "secondary.dark", // フォーカス時のラベル色
+  },
+  "& .MuiOutlinedInput-root": {
+    "&.Mui-focused fieldset": {
+      borderColor: "secondary.dark", // フォーカス時のアウトライン色
+    },
+  },
+};
+
+export const buttonSxContained = {
+  borderColor: "secondary.main",
+  backgroundColor: "secondary.main", // 通常時の背景色
+  "&:hover": {
+    backgroundColor: "secondary.dark", // ホバー時の背景色
+  },
+};
+
+export const buttonSxOutlined = {
+  color: "secondary.main",
+  borderColor: "secondary.main",
+  "&:hover": {
+    borderColor: "secondary.dark",
+    color: "secondary.dark",
+  },
+};
