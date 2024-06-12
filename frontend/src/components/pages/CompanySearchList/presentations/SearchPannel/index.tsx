76b6e395fe08e8adb0fa45d63d182b4703e76182
Shunnie816 <ponyo.shun0816@gmail.com>
2024-06-13 07:42:59 +0900

    modify serach params

--- frontend/src/components/pages/CompanySearchList/presentations/SearchPannel/index.tsx
+++ frontend/src/components/pages/CompanySearchList/presentations/SearchPannel/index.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/parts/Button";
 import styles from "./index.module.scss";
 import { Controller, useFormContext } from "react-hook-form";
 import { textFieldSx } from "@/components/themes/styleSx";
+import { SearchParamType } from "../../containers/formSchema";
 
 const sidePannelSx = {
   borderRadius: 2,
@@ -12,7 +13,8 @@ const sidePannelSx = {
 };
 
 export const SearchPannel = () => {
-  const { control, getValues, setValue, trigger } = useFormContext();
+  const { control, getValues, setValue, trigger } =
+    useFormContext<SearchParamType>();
 
   const handleBlur = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
@@ -32,25 +34,85 @@ export const SearchPannel = () => {
         <Typography variant="body2" fontWeight="bold">
           以下の条件で絞り込み中
         </Typography>
-        <div className={styles.searchWrapper}>
-          <div className={styles.option}>
-            <Typography component="p">会社名</Typography>
-            <Controller
-              control={control}
-              name="name"
-              render={({ field }) => (
-                <TextField
-                  {...field}
-                  label="会社名を入力"
-                  value={getValues("name")}
-                  size="small"
-                  onBlur={() => handleBlur}
-                  sx={textFieldSx}
-                />
-              )}
-            />
+        <form className={styles.form}>
+          <div className={styles.searchWrapper}>
+            <div className={styles.option}>
+              <Typography component="p">会社名</Typography>
+              <Controller
+                control={control}
+                name="name"
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    label="会社名を入力"
+                    value={getValues("name")}
+                    size="small"
+                    onBlur={() => handleBlur}
+                    sx={textFieldSx}
+                  />
+                )}
+              />
+            </div>
           </div>
-        </div>
+          <div className={styles.searchWrapper}>
+            <div className={styles.option}>
+              <Typography component="p">法人番号</Typography>
+              <Controller
+                control={control}
+                name="companyId"
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    label="法人番号を入力"
+                    value={getValues("companyId")}
+                    size="small"
+                    onBlur={() => handleBlur}
+                    sx={textFieldSx}
+                  />
+                )}
+              />
+            </div>
+          </div>
+          <div className={styles.searchWrapper}>
+            <div className={styles.option}>
+              <Typography component="p">業界・業種</Typography>
+              <Controller
+                control={control}
+                name="businessType"
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    label="業界・業種を入力"
+                    value={getValues("businessType")}
+                    size="small"
+                    onBlur={() => handleBlur}
+                    sx={textFieldSx}
+                  />
+                )}
+              />
+            </div>
+          </div>
+          <div className={styles.searchWrapper}>
+            <div className={styles.option}>
+              <Typography component="p">都道府県</Typography>
+              <Controller
+                control={control}
+                name="prefecture"
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    label="都道府県を入力"
+                    value={getValues("prefecture")}
+                    size="small"
+                    onBlur={() => handleBlur}
+                    sx={textFieldSx}
+                  />
+                )}
+              />
+            </div>
+          </div>
+          <Button variant="contained">検索</Button>
+        </form>
       </Box>
     </div>
   );
