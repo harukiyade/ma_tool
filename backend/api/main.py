c0c941f6c7979bda032a4499974f68fdb5eddecb
Shunnie816 <ponyo.shun0816@gmail.com>
2024-06-21 08:48:03 +0900

    enable python to return sample data

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,21 +1,21 @@
-from fastapi import FastAPI, HTTPException, Depends, Query
+from fastapi import FastAPI, HTTPException, Depends, Query, Request
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, Dict, Any, List
 import requests
+import yaml
 
 app = FastAPI()
-# TODO: api_tokenは環境ファイルに移動する。
+# TODO: api_tokenは環境ファイルに移動する。(firebase)
 api_token = "6IGiKYADLPU7YdsqRHPOtSWU8mJcyAgm"
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
     allow_credentials=True,
-    allow_methods=["GET"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 class QueryParams(BaseModel):
     name: str = Query(..., description="The name of the company")
@@ -53,6 +53,49 @@ class QueryParams(BaseModel):
     page: Optional[str] = Query(None, description="")
     limit: Optional[str] = Query(None, description="")
 
+# FEに渡す用のAPI
+@app.post("/api/companies")
+async def get_companies(request: Request):
+    params = await request.json()
+    print("Received request:", params)  # デバッグ用ログ
+    query_params = QueryParams(**params)
+    results = query_db(query_params.dict(exclude_none=True))
+    return results
+
+def query_db(db_params: Dict[str, Any]) -> List[Dict[str, Any]]:
+    # TODO: DBからparamsの条件に合ったデータを取得するクエリを実行する
+    
+    sample_data = [
+    {
+      "corporate_number": "1010001084833",
+      "postal_code": "1000005",
+      "location": "東京都千代田区丸の内２丁目４番１号",
+      "name": "株式会社パーソル総合研究所",
+      "status": "閉鎖",
+      "number_of_activity": "0",
+      "update_date": "2017-01-25T00:00:00+09:00"
+    },
+    {
+      "corporate_number": "1010001121801",
+      "postal_code": "1040053",
+      "location": "東京都中央区晴海１丁目８番５－２８０３号",
+      "name": "パーソルホールディングス株式会社",
+      "status": "-",
+      "number_of_activity": "0",
+      "update_date": "2020-02-13T00:00:00+09:00"
+    },
+    ]
+    return sample_data
+
+# OpenAPI仕様をFastAPIアプリケーションに追加
+with open("openapi.v1.yaml", "r") as f:
+    openapi_spec = yaml.safe_load(f)
+
+@app.get("/openapi.json", include_in_schema=False)
+async def get_openapi():
+    return app.openapi_schema
+
+# 外部APIの呼び出し
 @app.get("/api/corporate")
 async def get_corporate_data(params: QueryParams = Depends()):
     try:
