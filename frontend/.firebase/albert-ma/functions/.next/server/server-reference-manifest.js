12bae38759a2765100d63c42d4fcd5f1c9f2eba7
Shunnie816 <92611626+Shunnie816@users.noreply.github.com>
2024-06-26 07:41:05 +0900

    Delete frontend/.firebase directory

--- frontend/.firebase/albert-ma/functions/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"a1AYYheF5enNmVBQYYmlxiqiagvbUTf5zHQtCbW6fl8=\"}"
--- frontend/.firebase/albert-ma/functions/next.config.mjs
+++
@@ -1,8 +0,0 @@
-// next.config.mjs
-var nextConfig = {
-  output: "standalone"
-};
-var next_config_default = nextConfig;
-export {
-  next_config_default as default
-};
